7e587d73d00450add81c6ad1a7abb3bc8ff4b40c
Henrique Franco Dalgalarrondo <62577350+henriquedalga@users.noreply.github.com>
2023-06-17 16:19:26 -0300

    Update read.py

--- src/read.py
+++ src/read.py
@@ -3,6 +3,8 @@ from afd import AFD
 import pandas as pd
 
 
+def has_char(string, char):
+    return char in string
 
 def read_AFD(AFD_file): 
 
@@ -10,13 +12,31 @@ def read_AFD(AFD_file):
     AFD_table = pd.read_csv(AFD_file, delimiter='\t')
 
     #pega os estados inicial e final
-    Qi = AFD_table.iloc[0,0] 
-    Qf = AFD_table[AFD_table['&'].str.contains('\*', regex=False)]
+    cont = 0
+    Qi = 0
+    for i in AFD_table['&']:
+        if has_char(i,'!') == True:
+            AFD_table.at[cont, '&'] = i[1:]
+            if has_char(i,'!') == True:
+                Qi = int(i[2:])
+                #Qi.append(i[2:])
+            else:
+                Qi = int(i[1:])
+                #Qi.append(i[1:])
+        cont +=1
+    
+    cont = 0
+    Qf = []
+    for i in AFD_table['&']:
+        if has_char(i,'*') == True: # Verificar se algum valor contém o caractere "*"
+            Qf.append(int(i[1:]))
+            AFD_table.at[cont, '&'] = i[1:]
+        cont +=1
     # pega o alfabeto
     sigma = [] 
     for column in AFD_table.columns:
-        sigma.append(column)
-    sigma = sigma[1:]
+        if column != '&':
+            sigma.append(column)
     # pega os estados
     Qs = AFD_table['&'].unique()
     Qs = Qs[1:]
@@ -27,17 +47,18 @@ def read_AFD(AFD_file):
         aux = {}
         for column in AFD_table.columns:
             if column != '&':
-                aux.update({(row[0],column):row[column]})
+                aux.update({(int(row[0]),column):row[column]})
             #aux.append(row[column])
 
         delta.update(aux)
-    print(delta)
-
     return AFD(sigma,Qs,delta,Qi,Qf)
 
 # Specify the TSV file path
 AFD_file = 'data.tsv'
 D0 = read_AFD(AFD_file)
-D0.run("ab")
-
+#print(D0.delta)
+print(D0.run("bb"))
+#D1 = AFD({"a","b"},{0,1,2},{(0,"a"):0,(0,"b"):1,(1,"a"):2,(1,"b"):1,(2,"a"):2,(2,"b"):2}, 0, {0,1})
+#print(D1.run("ab"))
+#print(D0.delta[(0,'a')])
 
