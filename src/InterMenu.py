70d47436984a9cd8ad19ef06be8199dc355f1d17
S-Pz <savio_fr@outlook.com>
2023-06-30 15:08:31 -0300

    interface meu finalized

--- src/InterMenu.py
+++ src/InterMenu.py
@@ -4,15 +4,15 @@ import InterPilha
 
 window = tk.Tk()
 window.title("Escolha o tipo de automato")
-window.geometry("300x200")
+window.geometry("350x150")
 
 # AFD and AFN
 btn_AfnAfd = tk.Button(text="AFD ou AFN", command = InterAFDandAFN.openInterface)
-btn_AfnAfd.place(x=75, y=20, anchor=tk.CENTER)
+btn_AfnAfd.place(height=40, x=75, y=20)
 #btn_AfnAfd.grid(row=0, column=0, padx=75, pady=20)
 
 pushdown= tk.Button(text="Automanto de Pilha", command= InterPilha.openInterface)
-pushdown.grid(row=1, column=0, padx=75, pady=0)
+pushdown.place(height=40,x=75, y=60)
 
 #btn_AfnAfd.pack()
 #pushdown.pack()
